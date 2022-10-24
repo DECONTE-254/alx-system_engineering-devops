5cd5ed2d1a1cc6a960421cf9920c8d2e675dfdb8
DECONTE-254 <okothalex69@gmail.com>
2022-10-24 11:20:21 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _super - uppercase letters
+ * @c: char to check
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+
+	if (c > == 'A' && C < == 'z')
+		return (0);
+	else
+		return (1);
+}
